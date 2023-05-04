2e5dd0a256bf4861bc77a717f33f350fbc7e7e4d
ssmdbayzid <ssmdbayzid@gmail.com>
2023-05-05 00:34:26 +0600

    banner at process

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,22 +1,34 @@
+import { motion } from 'framer-motion'
 import React from 'react'
+
 
 const Banner = () => {
   const bg = "https://images.pexels.com/photos/5917850/pexels-photo-5917850.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
   return (
-    <section id='home'>
+    <section id='home' className="h-[100vh]">
      
-      <img src={bg} alt="" className="w-full h-[100vh] bg-cover" />
-      <div className="absolute top-0 left-0 h-full w-full bg-slate-500/40  ">
+      <img src={bg} alt="" className="w-full h-[100vh]  bg-cover" />
+      <div className="absolute top-0 left-0  h-[100vh] w-full bg-slate-500/40  ">
       <div className="justify-center items-center h-full p-5 flex flex-col ">
-        <p className="text-5xl text-center">
+
+        <motion.p 
+        initial="hidden"
+        whileInView="visible"
+        viewport={{once:1, amount:0.5 }}
+        transition={{duration: 0.5}}
+        variants={{
+          hidden:{opacity: 0, y: 50},
+          visible: {opacity:1, y:0}
+        }}
+        className="text-5xl text-center text-white font-extrabold selection:bg-yellow-400 selection:text-white">
             FASHION ALWAYS CHANGE
-        </p>
-        <p className="mt-10 text-lg text-center">
+        </motion.p>
+        <p className="mt-10 text-lg text-center  text-white">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores, qui?
         </p>
         <div className="z-10 space-x-6">
-          <button className="relative border-2 uppercase mt-10 px-10 py-3 rounded-tr-[25px] font-semibold bg-transparent text-yellow-400 transition-colors before:absolute">
-          Shart Shoping
+          <button className="relative mt-5 border-2 px-10 py-3 font-medium rounded-tr-[27px] text-white uppercase bg-transparent transition-colors before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-yellow-400 before:content-[''] before:rounded-tr-[25px] before:scale-x-0 before:origin-top-left hover:before:scale-x-100 before:transition-transform before:duration-300 hover:text-gray-900 before:z-[-1] ">
+          Start Shoping
           </button>
         </div>
       </div>
