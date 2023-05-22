6aadee581dbe62b07c83df62b7cce3ee678eb084
ssmdbayzid <ssmd.bayzid@gmail.com>
2023-05-22 20:42:53 +0600

    purchase page correction

--- src/pages/Purchase/Purchase.jsx
+++ src/pages/Purchase/Purchase.jsx
@@ -19,16 +19,17 @@ const Purchase = () => {
 
   // const {name, description, price, img, rating} = product;
 
+
   
   return (
     <>
      {product && <section class="text-gray-600 body-font overflow-hidden">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="lg:w-4/5 mx-auto flex flex-wrap">
+  <div class="container  py-24 mx-auto w-5/6">
+    <div class="lg:w-4/5 w-full mx-auto flex flex-wrap ">
       <div>
-      <img alt="ecommerce" class=" w-[400px] max-w-[400px] lg:h-full  object-cover object-center rounded" src={product.img}/>
+      <img alt="ecommerce" class=" w-[400px] max-w-[400px]  border-2 object-cover object-center rounded" src={product.img}/>
       </div>
-      <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+      <div class="lg:w-1/2 mx-auto w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
         <h2 class="text-sm title-font text-gray-500 tracking-widest">BRAND NAME</h2>
         <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">{product.name}</h1>
         <div class="flex mb-4">
@@ -68,6 +69,11 @@ const Purchase = () => {
             </a>
           </span>
         </div>
+        <p class="leading-relaxed flex font-bold">In Stock &nbsp;  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="green" class="w-6 h-6 text-bold">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+</svg> </p>
+
+        <h2 class="text-lg mt-2 title-font text-gray-500 tracking-widest">Description</h2>
         <p class="leading-relaxed">{product.description}</p>
         <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
           <div class="flex">
@@ -95,7 +101,7 @@ const Purchase = () => {
         </div>
         <div class="flex">
           <span class="title-font font-medium text-2xl text-gray-900">${product.price}.00</span>
-          <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
+          <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Add to cart</button>
           <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
             <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
               <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
